208b46f784be22cca24d92a913f8b9dc1738c275
P-r-a-n-a-v-N-a-i-r <86367423+P-r-a-n-a-v-N-a-i-r@users.noreply.github.com>
2021-06-28 20:35:16 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <Footer>
+      <p> Copyright by Pranav @ {new Date().getFullYear()}</p>
+    </Footer>
+  );
+}
+
+export default Footer;
